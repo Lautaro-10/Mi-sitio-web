40eb4878466c927adf03b9bb3ceba06584d795d8
Lautaro-10 <laucadelago123@gmail.com>
2024-10-24 13:11:49 -0300

    Mejora desciripcion y carrousel

--- funciones.js
+++ funciones.js
@@ -1,7 +1,17 @@
-const imagenes= ['/imagenes/bariloche.jfif','/imagenes/brasil.jfif','/imagenes/costa rica.jfif','/imagenes/marruecos.jfif']
-let indice=0;
+const imagenes = [
+    'imagenes/bariloche.jfif',
+    'imagenes/brasil.jfif',
+    'imagenes/costa-rica.jfif',  
+    'imagenes/marruecos.jfif'
+];
+let indice = 0;
 
 document.getElementById('siguiente').addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % images.length;
-    document.getElementById('carouselImagen').src = imagenes[indice];
+    indice = (indice + 1) % imagenes.length;  
+    document.getElementById('imagencarrousel').src = imagenes[indice];
+});
+
+document.getElementById('anterior').addEventListener('click', () => {
+    indice = (indice - 1 + imagenes.length) % imagenes.length;
+    document.getElementById('imagencarrousel').src = imagenes[indice];
 });
