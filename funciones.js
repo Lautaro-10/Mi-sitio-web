ec8cb195332f2aeef9ee5804c46e4bb4eabf116e
Lautaro-10 <laucadelago123@gmail.com>
2024-10-25 16:29:23 -0300

    cambios css y js

--- funciones.js
+++ funciones.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const imagenes = [
+        'imagenes/playa.webp',
+        'imagenes/nieve.jpg',
+        'imagenes/selva.jpg', 
+        'imagenes/desierto.jpg'
+    ];
+    let indice = 0;
+    
+    document.getElementById('siguiente').addEventListener('click', () => {
+        indice = (indice + 1) % imagenes.length; 
+        document.getElementById('imagencarrousel').src = imagenes[indice];
+    });
+    
+    document.getElementById('anterior').addEventListener('click', () => {
+        indice = (indice - 1 + imagenes.length) % imagenes.length;
+        document.getElementById('imagencarrousel').src = imagenes[indice];
+    });
+    
     const formulario = document.getElementById('formulario');
 
     formulario.addEventListener('submit', function(event) {
