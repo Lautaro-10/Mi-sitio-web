769090c259c4f1f0940c9de6ea06105388843a47
Lautaro-10 <laucadelago123@gmail.com>
2024-11-06 23:34:47 -0300

    muestra de feedback

--- funciones.js
+++ funciones.js
@@ -36,10 +36,10 @@ const formulario = document.getElementById("formulario")
             formularioValido = false;
         }
 
-      /*  if (!/^[0-9]{10}$/.test(telefono)) {
+        if (!/^[0-9]{10}$/.test(telefono)) {
             errorTelefono.textContent = "El teléfono debe tener 10 dígitos.";
             formularioValido = false;
-        } */
+        } 
 
         if (isNaN(edad) || edad < 18) {
             errorEdad.textContent = "La edad debe ser mayor o igual a 18 años.";
@@ -52,13 +52,31 @@ const formulario = document.getElementById("formulario")
         }
 
         if (formularioValido) {
-            alert("Formulario enviado correctamente.");
-            feedback.textContent = "¡Formulario enviado correctamente! Gracias por contactarnos.";
-            feedback.style.color = "green";
-            document.getElementById('nombre').value= "";
-            document.getElementById('email').value= "";
-            document.getElementById('telefono').value= "";
-            document.getElementById('edad').value= "";
-            document.getElementById('provincias').value= "";
+            console.log(nombre,email,edad,provincia);
+            
+            const datosDiv = document.createElement('div');
+            datosDiv.innerHTML = '<h3>Datos Ingresados:</h3>';
+            const nombreP = document.createElement('p');
+            nombreP.innerHTML = `<strong>Nombre:</strong> ${nombre}`;
+            datosDiv.appendChild(nombreP);
+            const emailP = document.createElement('p');
+            emailP.innerHTML = `<strong>Correo Electrónico:</strong> ${email}`;
+            datosDiv.appendChild(emailP);
+            const telefonoP = document.createElement('p');
+            telefonoP.innerHTML = `<strong>Teléfono:</strong> ${telefono}`;
+            datosDiv.appendChild(telefonoP);
+            const edadP = document.createElement('p');
+            edadP.innerHTML = `<strong>Edad:</strong> ${edad}`;
+            datosDiv.appendChild(edadP);
+        
+            const provinciaP = document.createElement('p');
+            provinciaP.innerHTML = `<strong>Provincia:</strong> ${provincia}`;
+            datosDiv.appendChild(provinciaP);
+            feedback.appendChild(datosDiv);
+        
+            document.getElementById('formulario').reset();
+            
         }
+        
     });
+   
